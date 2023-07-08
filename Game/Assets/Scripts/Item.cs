d43514122a0de174b0f57aba559b88ccbcd3ef29
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2023-07-08 19:54:07 +0200

    fixed portals

--- Game/Assets/Scripts/Item.cs
+++ Game/Assets/Scripts/Item.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Item : MonoBehaviour
@@ -39,10 +40,83 @@ public class Item : MonoBehaviour
             var mirror = other.GetComponent<Mirror>();
             var rigidbody = GetComponent<Rigidbody2D>();
             var previousVelocity = rigidbody.velocity;
-            var newPosition = mirror.Other.transform.position + 2 * GetComponent<Collider2D>().bounds.extents * Math.Sign(previousVelocity.x);
-            transform.SetPositionAndRotation(newPosition, transform.rotation);
-            //rigidbody.velocity = previousVelocity;
+
+            /*
+            //red (m1 rotation)
+            var red = mirror.transform.rotation.eulerAngles.z;
+            Debug.Log(red); //60
+            //green
+            var green = 90 - red;
+            //yellow
+            var yellow = Quaternion.FromToRotation(Vector3.up, new Vector3(previousVelocity.x, previousVelocity.y));
+            Debug.Log(yellow.eulerAngles.z); //0.745
+            //blue
+            var blue = red + 90 - yellow.eulerAngles.z;
+            Debug.Log(blue); //149.25
+            //m2 rotation
+            var pink = mirror.Other.transform.rotation.eulerAngles.z;
+            Debug.Log(pink); //180
+            //target angle
+            var target = pink + green;
+            Debug.Log(target); //210
+            //diff
+            var diff = Quaternion.Angle(yellow, quaternion.Euler(0, 0, target));
+            var diffQ = Quaternion.RotateTowards(yellow, quaternion.Euler(0, 0, target), 360);*/
+            
+            //m1Tilt
+            var m1Tilt = mirror.transform.rotation.eulerAngles.z;
+            Debug.Log("M1 Tilt: " + m1Tilt); //60
+            //velocityAngle
+            var velocityAngleQ = Quaternion.FromToRotation(Vector3.up, new Vector3(previousVelocity.x, previousVelocity.y));
+            Debug.Log("Velocity Angle: " + velocityAngleQ.eulerAngles.z); 
+            //alpha (Einfallswinkel = velocityAngle - m1 rotation)
+            var alpha = velocityAngleQ.eulerAngles.z - m1Tilt;
+            if (alpha > 90)
+                alpha = 180 - alpha;
+            if (alpha < 0)
+                alpha += 360;
+            Debug.Log("Einfallswinkel: " + alpha);
+            //m2Tilt
+            var m2Tilt = mirror.Other.transform.rotation.eulerAngles.z;
+            Debug.Log("M2 Tilt: " + m2Tilt);
+            //target
+            var target = m2Tilt - alpha;
+            Debug.Log("target angle: " + target);
+            //diff
+            var diff = target - velocityAngleQ.eulerAngles.z;//Quaternion.Angle(velocityAngleQ, quaternion.Euler(0, 0, target));
+            var diffQ = Quaternion.RotateTowards( velocityAngleQ, Quaternion.Euler(0, 0, target), 360);
+
+            Debug.Log("Diff to rotate: " + diff); //151.36
+            Debug.DrawRay(mirror.Other.transform.position, Quaternion.Euler(0,0,diff) * new Vector3(previousVelocity.x, previousVelocity.y), Color.yellow, 100);
+            
+            //var outVector = diffQ * new Vector3(previousVelocity.x, previousVelocity.y).normalized * 3f;
+            //sin alpha * h = d
+            var outVector = Quaternion.Euler(0,0,diff) * new Vector3(previousVelocity.x, previousVelocity.y).normalized * (float)(1.5 / Math.Sin(alpha));
+            Debug.DrawRay(mirror.transform.position, new Vector3(previousVelocity.x, previousVelocity.y), Color.black, 100);
+            Debug.DrawRay(mirror.Other.transform.position, outVector, Color.white, 100);
+            Debug.Log("t");
+            transform.SetPositionAndRotation(mirror.Other.transform.position + outVector, transform.rotation);
             rigidbody.gravityScale *= -1;
+            rigidbody.velocity = Quaternion.Euler(0, 0, target) * rigidbody.velocity;
+            Debug.Log("v: " + rigidbody.velocity);
+
+            //var angle2 = Quaternion.Angle(mirror.transform.rotation, mirror.Other.transform.rotation);
+            //Debug.Log(mirror.Other.transform.rotation);
+            //Debug.Log(angle2);
+            //Debug.DrawRay(mirror.Other.transform.position, Quaternion.Euler(0, 0, angle2) * new Vector3(previousVelocity.x, previousVelocity.y), Color.cyan, 100);
+            //Debug.Log(Quaternion.AngleAxis(Quaternion.Angle(Quaternion.FromToRotation(Vector3.right, previousVelocity), mirror.Other.transform.rotation), Vector3.forward));
+            //Debug.DrawRay(mirror.transform.position, mirror.transform.right - new Vector3(previousVelocity.x, previousVelocity.y), Color.yellow, 100);
+            //Debug.DrawRay(mirror.Other.transform.position, mirror.transform.right - new Vector3(previousVelocity.x, previousVelocity.y) - mirror.Other.transform.right, Color.yellow, 100);
+            //Debug.DrawRay(mirror.transform.position,  mirror.transform.rotation * new Vector3(previousVelocity.x, previousVelocity.y), Color.cyan, 100);
+            //Debug.DrawRay(mirror.Other.transform.position,  mirror.Other.transform.rotation * new Vector3(previousVelocity.x, previousVelocity.y), Color.cyan, 100);
+            //Debug.DrawRay(mirror.Other.transform.position,  mirror.Other.transform.rotation * new Vector3(previousVelocity.x, previousVelocity.y), Color.cyan, 100);
+            //Debug.DrawRay(mirror.Other.transform.position,  (Quaternion.RotateTowards(mirror.transform.rotation, mirror.Other.transform.rotation, 360)) * new Vector3(previousVelocity.x, previousVelocity.y), Color.magenta, 100);
+            /*var outVector = (Quaternion.Euler(0, 0, angle2) * new Vector3(previousVelocity.x, previousVelocity.y)).normalized * 3f;
+            Debug.DrawRay(mirror.transform.position, new Vector3(previousVelocity.x, previousVelocity.y), Color.black, 100);
+            Debug.DrawRay(mirror.Other.transform.position, outVector, Color.white, 100);
+            Debug.Log("t");
+            transform.SetPositionAndRotation(mirror.Other.transform.position + outVector, transform.rotation);
+            rigidbody.gravityScale *= -1;*/
         }
     }
 }
--- Game/Assets/Scripts/Mirror.cs
+++ Game/Assets/Scripts/Mirror.cs
@@ -8,12 +8,13 @@ public class Mirror : MonoBehaviour
     public Mirror Other;
     void Start()
     {
-        
+        //Application.targetFrameRate = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Debug.DrawRay(transform.position, transform.up, Color.green);
+        Debug.DrawRay(transform.position, transform.right, Color.red);
     }
 }
